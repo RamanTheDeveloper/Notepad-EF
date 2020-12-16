336e1ba93ed67774fe6406240c8eda15328a1dce
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2020-12-16 21:06:09 +0100

    Add project files.

---
+++ Notepad/ManagerDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleNotepad
+{
+    /// <summary>
+    /// Interaction logic for ManagerDialog.xaml
+    /// </summary>
+    public partial class ManagerDialog : Window
+    {
+        public ManagerDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Notepad/ViewModels/HelpViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace SimpleNotepad.ViewModels
+{
+    //About tab --> who created this etc.
+    public class HelpViewModel : ObservableObject
+    {
+        public ICommand HelpCommand { get; }
+
+        public HelpViewModel()
+        {
+            HelpCommand = new RelayCommand(DisplayAbout);
+        }
+
+        private void DisplayAbout()
+        {
+            //Will open help dialog
+        }
+    }
+}
